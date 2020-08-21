116ade0962c8d8f65fd2cd57e101cc26271e61c9
Nicholas Henrique Brossa <nicholasbrossa@hotmail.com>
2020-08-21 16:30:03 -0300

    If e Else

--- exercicio1/exe1.py
+++ exercicio1/exe1.py
@@ -1,1 +1,12 @@
-print('Hello World')
+# faca um programa que receba a idade
+# e veja se ele e maior ou menor de idade
+
+print('Ola')
+
+nome = input('Digite seu Nome: ')
+idade = int(input('Digite sua Idade: '))
+
+if idade >= 18:
+    print('Voce eh maior de idade!')
+else:
+    print('Voce eh menor de idade!')
