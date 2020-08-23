dd2b4286dd5cee6dbb52d44450c54f041c1e0353
Nicholas Henrique Brossa <nicholasbrossa@hotmail.com>
2020-08-23 10:32:17 -0300

    Operadores aritimeticos

---
+++ exercicio1/exe6.py
@@ -0,0 +1,9 @@
+pri_numero = 6
+seg_numero = 2
+print('A soma desses numbers eh: ', pri_numero + seg_numero)
+print('O expoente desses numbers eh: ', pri_numero ** seg_numero)
+print('A subtracao desses numbers eh: ', pri_numero - seg_numero)
+print('A divisao desses numbers eh: ', pri_numero / seg_numero)
+print('A multiplicacao desses numbers eh: ', pri_numero * seg_numero)
+print('A parte inteira desses numbers eh: ', pri_numero // seg_numero)
+print('O modulo desses numbers eh: ', pri_numero % seg_numero)
