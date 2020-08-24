0d199c6682936c39982799ddc9139d17511cc17a
Nicholas Henrique Brossa <nicholasbrossa@hotmail.com>
2020-08-24 10:32:23 -0300

    int e float

---
+++ exercicio1/exe7.py
@@ -0,0 +1,5 @@
+first_number = input('Digite o primeiro numero: ')
+second_number = input('Digite o segundo numero: ')
+
+print(int(first_number) + int(second_number))
+print(float(first_number) + int(second_number))
